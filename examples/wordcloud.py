26712e28ef3890cb462c6d80c87b3ddda21b6e5e
Lucy Park <me@lucypark.kr>
2014-06-11 14:23:04 +0900

    Add wordcloud example

---
+++ examples/wordcloud.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+import urllib
+import random
+import webbrowser
+
+import konlpy
+from lxml import html
+import pytagcloud
+
+
+r = lambda: random.randint(0,255)
+color = lambda: (r(), r(), r())
+
+def get_bill_text(billnum):
+    url = 'http://pokr.kr/bill/%s/text' % billnum
+    response = urllib.urlopen(url).read().decode('utf-8')
+    page = html.fromstring(response)
+    text = page.xpath(".//div[@id='bill-sections']/pre/text()")[0]
+    return text
+
+def get_tags(text, h, ntags=50, multiplier=10):
+    nouns = h.nouns(text)
+    count = Counter(nouns)
+    return [{ 'color': color(), 'tag': n, 'size': c*multiplier }\
+                for n, c in count.most_common(ntags)]
+
+def draw_cloud(tags, filename, fontname='NanumGothic_Coding'):
+    pytagcloud.create_tag_image(tags, filename, fontname=fontname)
+    webbrowser.open(filename)
+
+text = get_bill_text('1904882')
+tags = get_tags(text, konlpy.Hannanum())
+draw_cloud(tags, 'wordcloud.png')
