95ee5d03892d7fc1f1f92375d0939a7e428e24bf
Lucy Park <me@lucypark.kr>
2014-08-16 04:22:28 +0900

    Add custom css file

--- docs/conf.py
+++ docs/conf.py
@@ -134,6 +134,7 @@ html_logo = '_static/konlpy.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = 'konlpy.css'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
