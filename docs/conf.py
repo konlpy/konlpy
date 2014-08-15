1ce0a167c251bb62460b3f457981763e94069a40
Lucy Park <me@lucypark.kr>
2014-08-16 02:59:53 +0900

    Eliminate document doctests

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ from konlpy import __version__
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
