68c868be21b785e2a43e653c906bb93d813c8693
Lucy Park <me@lucypark.kr>
2014-08-22 00:50:41 +0900

    Modify docs: concordance

--- konlpy/utils.py
+++ konlpy/utils.py
@@ -31,14 +31,24 @@ class UnicodePrinter(pp.PrettyPrinter):
 def concordance(phrase, text):
     """Find concordances of a phrase in a text.
 
-    .. code-block: python
+    .. code-block:: python
 
-        from konlpy.corpus import kolaw
-        from konlpy.tag import Mecab
-        from konlpy import utils
-
-        constitution = kolaw.open('constitution.txt').read()
-        utils.concordance(u'대통령', constitution)
+        >>> from konlpy.corpus import kolaw
+        >>> from konlpy.tag import Mecab
+        >>> from konlpy import utils
+        >>> constitution = kolaw.open('constitution.txt').read()
+        >>> utils.concordance(u'대한민국', constitution)
+        0       대한민국헌법 유구한 역사와
+        9       대한국민은 3·1운동으로 건립된 대한민국임시정부의 법통과 불의에
+        98      총강 제1조 ① 대한민국은 민주공화국이다. ②대한민국의
+        100     ① 대한민국은 민주공화국이다. ②대한민국의 주권은 국민에게
+        110     나온다. 제2조 ① 대한민국의 국민이 되는
+        126     의무를 진다. 제3조 대한민국의 영토는 한반도와
+        133     부속도서로 한다. 제4조 대한민국은 통일을 지향하며,
+        147     추진한다. 제5조 ① 대한민국은 국제평화의 유지에
+        787     군무원이 아닌 국민은 대한민국의 영역안에서는 중대한
+        1836    파견 또는 외국군대의 대한민국 영역안에서의 주류에
+        3620    경제 제119조 ① 대한민국의 경제질서는 개인과
     """
 
     terms = text.split()
