b45be344600cdd680c57d250b9195dfe173a5825
Lucy Park <me@lucypark.kr>
2014-12-19 22:00:23 +0900

    Add internal utils

--- konlpy/__init__.py
+++ konlpy/__init__.py
@@ -2,10 +2,11 @@ __title__ = 'KoNLPy'
 __version__ = '0.4.0'
 __author__ = 'Lucy Park'
 __license__ = 'GPL v3'
-__copyright__ = 'Copyright 2014 Lucy Park'
+__copyright__ = 'Copyright 2015 Lucy Park'
 
 from .downloader import download
 from .jvm import init_jvm
 from . import corpus
 from . import data
+from . import internals
 from . import tag
---
+++ konlpy/internals.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+
+import os
+import stat
+
+
+def is_writable(path):
+    if not os.path.exists(path):
+        return False
+
+    # If we're on a posix system, check its permissions.
+    if hasattr(os, 'getuid'):
+        statdata = os.stat(path)
+        perm = stat.S_IMODE(statdata.st_mode)
+        # is it world-writable?
+        if (perm & 0o002):
+            return True
+        # do we own it?
+        elif statdata.st_uid == os.getuid() and (perm & 0o200):
+            return True
+        # are we in a group that can write to it?
+        elif (statdata.st_gid in [os.getgid()] + os.getgroups()) \
+            and (perm & 0o020):
+            return True
+        # otherwise, we can't write to it.
+        else:
+            return False
+    # Otherwise, we'll assume it's writable.
+    return True
