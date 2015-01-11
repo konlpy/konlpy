2fee854114ca2f28627845657d59a0b862452ca8
Lucy Park <me@lucypark.kr>
2015-01-11 14:44:58 +0900

    Silence Komoran() warnings

--- konlpy/tag/__init__.py
+++ konlpy/tag/__init__.py
@@ -3,11 +3,7 @@ import warnings
 
 from ._hannanum import Hannanum
 from ._kkma import Kkma
-if sys.version_info[0] < 3:
-    warnings.filterwarnings("once")
-    warnings.warn("Your system does not support KOMORAN", ImportWarning)
-else:
-    from ._komoran import Komoran
+from ._komoran import Komoran
 try:
     from ._mecab import Mecab
 except ImportError:
