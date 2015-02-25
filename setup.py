38742de487737429fa4fd58ca7268436eea0581e
Lucy Park <me@lucypark.kr>
2015-02-25 20:38:22 +0900

    Add license meta data field to setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ KoNLPy is not just to create another, but to unify and build upon their shoulder
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         ],
+    license='GPL v3+',
     packages=find_packages(),
     package_data={'konlpy': [
         'data/*/*/*.txt',
