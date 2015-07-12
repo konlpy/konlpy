f6b2577dea5aa3a7ef050267ebd9563266b253e3
Lucy Park <me@lucypark.kr>
2015-07-13 00:09:53 +0900

    Render morph tests with matplotlib

--- docs/morph.py
+++ docs/morph.py
@@ -16,9 +16,10 @@ def tagging(tagger, text):
         print "Uhoh,", e
     return r
 
+
 def measure_time(taggers, mult=6):
     doc = kolaw.open('constitution.txt').read()*6
-    data = [[''] + taggers]
+    data = [['n'] + taggers]
     for i in range(mult):
         doclen = 10**i
         times = [time()]
@@ -33,6 +34,7 @@ def measure_time(taggers, mult=6):
         print
     return data
 
+
 def measure_accuracy(taggers, text):
     print '\n%s' % text
     result = []
@@ -44,7 +46,32 @@ def measure_accuracy(taggers, text):
     return result
 
 
+def plot(result):
+
+    from matplotlib import pylab as pl
+    import scipy as sp
+
+    if not result:
+        result = sp.loadtxt('morph.csv', delimiter=',', skiprows=1).T
+
+    x, y = result[0], result[1:]
+
+    for i in y:
+        pl.plot(x, i)
+
+    pl.xlabel('Number of characters')
+    pl.ylabel('Time (sec)')
+    pl.xscale('log')
+    pl.grid(True)
+    pl.savefig("images/time.png")
+    pl.show()
+
+
 if __name__=='__main__':
+
+    PLOT = False
+    MULT = 6
+
     examples = [u'아버지가방에들어가신다',  # 띄어쓰기
             u'나는 밥을 먹는다', u'하늘을 나는 자동차', # 중의성 해소
             u'아이폰 기다리다 지쳐 애플공홈에서 언락폰질러버렸다 6+ 128기가실버ㅋ'] # 속어
@@ -52,7 +79,7 @@ if __name__=='__main__':
     taggers = [t for t in dir(tag) if t[0].isupper()]
 
     # Time
-    data = measure_time(taggers, mult=6)
+    data = measure_time(taggers, mult=MULT)
     with open('morph.csv', 'w') as f:
         csvwrite(data, f)
 
@@ -62,3 +89,7 @@ if __name__=='__main__':
         result = map(lambda *row: [i or '' for i in row], *result)
         with open('morph-%s.csv' % i, 'w') as f:
             csvwrite(result, f)
+
+    # Plot
+    if PLOT:
+        plot(result)
