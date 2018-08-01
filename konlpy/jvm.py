7413e7153e39c8c88f2b29b454831a4b036d9ad5
lovit <soy.lovit@gmail.com>
2018-08-01 22:14:29 +0900

    Change 1Gb -> 1GB

--- konlpy/jvm.py
+++ konlpy/jvm.py
@@ -16,7 +16,7 @@ def init_jvm(jvmpath=None, max_heap_size=1024):
     """Initializes the Java virtual machine (JVM).
 
     :param jvmpath: The path of the JVM. If left empty, inferred by :py:func:`jpype.getDefaultJVMPath`.
-    :param max_heap_size: Maximum memory usage limitation (Megabyte). Default is 1024 (1Gb). If you set this value too small, you may got out of memory. We recommend that you set it 1024 ~ 2048 or more at least. However, if this value is too large, you may see inefficient memory usage.
+    :param max_heap_size: Maximum memory usage limitation (Megabyte). Default is 1024 (1GB). If you set this value too small, you may got out of memory. We recommend that you set it 1024 ~ 2048 or more at least. However, if this value is too large, you may see inefficient memory usage.
 
     """
 
