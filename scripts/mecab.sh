5dfaaf699b5067524b7495bf80383001891feef8
HaebinShin <sunsal0704@gmail.com>
2019-09-15 01:46:40 +0900

    fix to bug that after bash4.4 cognize tar to multiple directory

--- scripts/mecab.sh
+++ scripts/mecab.sh
@@ -64,7 +64,8 @@ install_automake(){
 
             # download and extract source
             curl -LO http://ftpmirror.gnu.org/autoconf/autoconf-latest.tar.gz
-            tar -zxvf autoconf-*
+            tar -zxvf autoconf-latest.tar.gz
+            rm autoconf-latest.tar.gz
 
             # configure, make, install --prefix=/usr/local
             cd autoconf*
