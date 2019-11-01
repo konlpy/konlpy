6b0f96e54ff46e2a19a0119cbaf39dbf3f046664
Minho Ryang <minhoryang@gmail.com>
2019-11-02 08:44:17 +0900

    Update test_komoran.py and test_openkoreantext.py to cover all methods.

--- test/test_komoran.py
+++ test/test_komoran.py
@@ -44,3 +44,7 @@ else:
              u'\uac00/VV',
              u'\uc790/EF',
              u'!/SF']
+
+    def test_komoran_morphs(komoran_instance, string):
+        assert komoran_instance.morphs(string) ==\
+            [u'\uaf43\uac00\ub9c8', u'\ud0c0', u'\uace0', u'\uac15\ub0a8', u'\uac00', u'\uc790', u'!']
